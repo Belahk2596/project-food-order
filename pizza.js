6c6d5786facabc8385a4ade2f46c27a1b76d998e
Khaleb Ikinepule <khaleb.ikinepule@hotmail.com>
2024-08-20 16:59:13 +1200

    initial commit

---
+++ pizza.js
@@ -0,0 +1,11 @@
+console.log("I love pizza")
+let pizzaOrder = "Ham and Cheese";
+let pizzaPrice = 10;
+console.log("New pizza order: " + pizzaOrder    + ". The price of the pizza is: $"  + pizzaPrice);
+let amountOfPizza = 5;
+let familyPizza = pizzaPrice * 2;
+let regularPizza = 10;
+let totalPrice;
+totalPrice = amountOfPizza * pizzaPrice;
+console.log("New pizza order: " + amountOfPizza + familyPizza + pizzaOrder + ". The total price of the pizza is: $"  + totalPrice);
+
