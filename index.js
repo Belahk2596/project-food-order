34e2ef9fe21c392b8f06e055b5c36b77fe1ad782
Khaleb Ikinepule <khaleb.ikinepule@hotmail.com>
2024-09-05 16:13:24 +1200

    javascript

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-console.log("I love pizza")
-let pizzaOrder = "Ham and Cheese";
-let pizzaPrice = 10;
-console.log("New pizza order: " + pizzaOrder    + ". The price of the pizza is: $"  + pizzaPrice);
-let amountOfPizza = 5;
-let familyPizza = "family";
-let regularPizza = "regular";
-let totalPrice;
-totalPrice = amountOfPizza * pizzaPrice;
-console.log("New pizza order: " + amountOfPizza + " " + familyPizza + " " + pizzaOrder + ". The total price of the pizza is: $"  + totalPrice);
 
+let pizzaOrder = {
+pizzaOne: "Pepperoni Pizza",
+pizzaTwo: "Meatlovers Pizza",
+pizzaThree: "Cheese Pizza"
+};
