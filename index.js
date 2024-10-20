0dc14620a904cf1de3a6c44fb1df4d18062c415e
Belahk2596 <khaleb.ikinepule@hotmail.com>
2024-10-20 22:27:19 +1300

    update total price

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-const order = {
-    pepperoni: {
-        regular: 12,
-        large: 16 
-    },
-    meatlovers: {
-        regular: 12,
-        large: 16
-    },
-    cheese: {
-        regular: 12,
-        large: 16
-    }
-}
-console.log(order);
+const pepperoniRegular = document.getElementById("pepperoni-regular");
+const pepperoniLarge = document.getElementById("pepperoni-large");
 
-function addToOrder(type, size) {
+const meatloversRegular = document.getElementById("meatlovers-regular");
+const meatloversLarge = document.getElementById("meatlovers-large");
 
-}
+const cheeseRegular = document.getElementById("cheese-regular");
+const cheeseLarge = document.getElementById("cheese-large");
+
+let totalPrice = document.getElementById("total-price");
+let totalCounter = 0;
+
+const totalString = 'Total Price: $'
+
+pepperoniRegular.addEventListener("click", function () {
+    totalCounter = totalCounter + 12;
+    totalPrice.innerText = `${totalString}${totalCounter}.00`;
+});
+
+pepperoniLarge.addEventListener("click", function () {
+    totalCounter = totalCounter + 16;
+    totalPrice.innerText = `${totalString}${totalCounter}.00`;
+});
+
+meatloversRegular.addEventListener("click", function () {
+    totalCounter = totalCounter + 12;
+    totalPrice.innerText = `${totalString}${totalCounter}.00`;
+});
+
+meatloversLarge.addEventListener("click", function () {
+    totalCounter = totalCounter + 12;
+    totalPrice.innerText = `${totalString}${totalCounter}.00`;
+});
+
+cheeseRegular.addEventListener("click", function () {
+    totalCounter = totalCounter + 12;
+    totalPrice.innerText = `${totalString}${totalCounter}.00`;
+});
+
+cheeseLarge.addEventListener("click", function () {
+    totalCounter = totalCounter + 12;
+    totalPrice.innerText = `${totalString}${totalCounter}.00`;
+});
